ab2841ba9198bc79df75d445829551b54ff9c28d
Jan Pipek <jan.pipek@gmail.com>
2017-10-13 22:46:55 +0200

    setup.pu

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import itertools
+from physt import __version__
+
+options = dict(
+    name='zbubliny_matcher',
+    version=__version__,
+    packages=find_packages(),
+    license='MIT',
+    description='Matchers for zbubliny',
+    long_description=__doc__.strip(),
+    # author='Jan Pipek',
+    # author_email='jan.pipek@gmail.com',
+    # url='https://github.com/janpipek/physt',
+    # package_data={"physt" : ["examples/*.csv"]},
+    install_requires = ['googletrans'],
+    #extras_require = {
+    #    'all' : ['dask', 'matplotlib', 'bokeh', 'folium']
+    #},
+    entry_points = {
+        'console_scripts' : [
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
+
+extras = options['extras_require']
+# extras['full'] = list(set(itertools.chain.from_iterable(extras.values())))
+setup(**options)
