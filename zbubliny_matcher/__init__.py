7966d2bafa574b398fd303f6922a56fe5439c5ac
Jan Pipek <jan.pipek@gmail.com>
2017-10-13 22:54:27 +0200

    ...

--- zbubliny_matcher/__init__.py
+++ zbubliny_matcher/__init__.py
@@ -9,4 +9,4 @@ def naive_matcher(text, keywords, text_language, keyword_language) -> float:
 def simple_matcher(text: str, keywords: List[str], text_language: str, keyword_language: str) -> float:
     translator = Translator()
     keywords_translated = [translator.translate(keyword, text_language, keyword_language).text for keyword in keywords]
-    return [int(keyword in text) for keyword in keywords_translated] / len(keywords)
+    return sum(int(keyword in text) for keyword in keywords_translated) / len(keywords)
