61afa54f5b16640e7f419bfad384db8ea9b773df
Jan Pipek <jan.pipek@gmail.com>
2017-10-14 02:37:50 +0200

    Update model + CLI

--- zbubliny_matcher/cli_interface.py
+++ zbubliny_matcher/cli_interface.py
@@ -7,20 +7,24 @@ import glob
 @click.option('-l', "--limit", default=-1, type=int)
 @click.option('-v', "--vector_dir", required=True)
 @click.option('-t', "--text_dir", required=True)
-@click.option('-s', "--source_language", default="cs", required=True)
-@click.option('-d', "--destination_language", required=True)
+@click.option('-k', "--keyword_language", default="cs", required=True)
+@click.option('-s', "--source_language", required=True)
+@click.option('-d', "--debug", is_flag=True)
 @click.argument("keywords", nargs=-1)
-def run_it(keywords, vector_dir, text_dir, source_language, destination_language, limit):
+def run_it(keywords, vector_dir, text_dir, keyword_language, source_language, limit, debug):
     from zbubliny_matcher.word2vec_matcher import SimpleWord2VecMatcher
 
-    m = SimpleWord2VecMatcher(debug=True)
-    m.load_language_model("it", os.path.join(vector_dir, "wiki.{0}.vec".format(destination_language)))
+    m = SimpleWord2VecMatcher(debug=debug)
+    print("Loading model...")
+
+    m.load_language_model("it", os.path.join(vector_dir, "wiki.{0}.vec".format(source_language)))
 
     for i, filename in enumerate(glob.glob(os.path.join(text_dir, "*.txt"))):
         with open(filename, "r") as f:
             text = f.read()
             print("Testing file {0}...".format((filename)))
-            score = m(text, keywords, destination_language, source_language)
+            score = m(text, keywords, source_language, keyword_language)
+            print("Achieved score: {0}".format(score))
 
         if limit > 0 and i >= limit - 1:
             break
--- zbubliny_matcher/word2vec_matcher.py
+++ zbubliny_matcher/word2vec_matcher.py
@@ -1,5 +1,6 @@
 from .chunkers import SimpleChunker
 from googletrans import Translator
+from typing import List
 import re
 
 
@@ -14,21 +15,25 @@ class SimpleWord2VecMatcher:
         self.translator = Translator()
         self.debug = debug
 
-    def add_language_model(self, language, model):
+    def add_language_model(self, language: str, model):
         self.models[language] = model
 
-    def load_language_model(self, language, path):
+    def load_language_model(self, language: str, path: str):
         from .model_loader import load_model
         model = load_model(path)
         self.add_language_model(language, model)
+        if self.debug:
+            print("Loaded model for {0} from {1}.".format(language, path))
 
-    def match_keyword_sentence(self, model, keyword, sentence):
-        result = self.model.wv.n_similarity([keyword], sentence)
+    def match_keyword_sentence(self, model, keyword, sentence: List) -> float:
+        if not keyword or not sentence:
+            return 0.0
+        result = model.wv.n_similarity([keyword], sentence)
         if self.debug:
             print("Matching {0} against {1} => {2}".format(keyword, sentence, result))
         return result
 
-    def match_keyword_text(self, model, keyword, sentences, text):
+    def match_keyword_text(self, model, keyword: str, sentences: List[List[str]], text: str) -> float:
         if keyword in text:
             return 1.0
         elif keyword in model.wv.vocab:
@@ -36,7 +41,7 @@ class SimpleWord2VecMatcher:
         else:
             return 0.0
 
-    def __call__(self, text, keywords, text_language, keyword_language):
+    def __call__(self, text: str, keywords: List[str], text_language: str, keyword_language: str) -> float:
         if not self.models:
             raise LanguageNotSupported("There is no model for language: {0}, please try loading at least one model first.".format(text_language))
         model = self.models.get(text_language)
