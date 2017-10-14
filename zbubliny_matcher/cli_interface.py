d08d523f23c456aa4bf417e6acf93c014d708739
Jan Pipek <jan.pipek@gmail.com>
2017-10-14 04:17:27 +0200

    Ngram matching

--- zbubliny_matcher/cli_interface.py
+++ zbubliny_matcher/cli_interface.py
@@ -13,18 +13,16 @@ import codecs
 @click.option('-d', "--debug", is_flag=True)
 @click.argument("keywords", nargs=-1)
 def run_it(keywords, vector_dir, text_dir, keyword_language, source_language, limit, debug):
-    from zbubliny_matcher.word2vec_matcher import SimpleWord2VecMatcher
+    from zbubliny_matcher.word2vec_matcher import SentenceVecMatcher
 
-    m = SimpleWord2VecMatcher(debug=debug)
-    print("Loading model...")
-    m.load_language_model("it", os.path.join(vector_dir, "wiki.{0}.vec".format(source_language)))
-    print("Model loaded.")
+    m = SentenceVecMatcher(debug=debug)
+    m.load_language_model(source_language, os.path.join(vector_dir, "wiki.{0}.vec".format(source_language)))
 
     for i, filename in enumerate(glob.glob(os.path.join(text_dir, "*.txt"))):
         with codecs.open(filename, "r", encoding="utf-8") as f:
             text = f.read()
             score = m(text, keywords, source_language, keyword_language)
-            print("{0}: {1}".format(filename, score))
+            print("{0}, {1}".format(filename, score))
 
         if limit > 0 and i >= limit - 1:
             break
@@ -32,3 +30,38 @@ def run_it(keywords, vector_dir, text_dir, keyword_language, source_language, li
 
 def run():
     run_it(auto_envvar_prefix="ZBUBLINY")
+
+
+
+@click.command()
+@click.option('-l', "--limit", default=-1, type=int)
+@click.option('-v', "--vector_dir", required=True)
+@click.option('-t', "--text_dir", required=True)
+@click.option('-k', "--keyword_language", default="cs", required=True)
+@click.option('-s', "--source_language", required=True)
+@click.option('-d', "--debug", is_flag=True)
+@click.argument("keywords", nargs=-1)
+def run_it_multi(keywords, vector_dir, text_dir, keyword_language, source_language, limit, debug):
+    from zbubliny_matcher.word2vec_matcher import SentenceVecMatcher, NgramVecMatcher
+
+    m = SentenceVecMatcher(debug=debug)
+    m.load_language_model(source_language, os.path.join(vector_dir, "wiki.{0}.vec".format(source_language)))
+
+    matchers = [m]
+    for i in range(1, 6):
+        n_matcher = NgramVecMatcher(i, debug=debug)
+        n_matcher.add_language_model(source_language, m.models[source_language])
+        matchers.append(n_matcher)
+
+    for i, filename in enumerate(glob.glob(os.path.join(text_dir, "*.txt"))):
+        with codecs.open(filename, "r", encoding="utf-8") as f:
+            text = f.read()
+            scores = [m(text, keywords, source_language, keyword_language) for m in matchers]
+            print("{0}, {1}".format(filename, ", ".join([str(score) for score in scores])))
+
+        if limit > 0 and i >= limit - 1:
+            break
+
+
+def run_multi():
+    run_it_multi(auto_envvar_prefix="ZBUBLINY")
--- zbubliny_matcher/word2vec_matcher.py
+++ zbubliny_matcher/word2vec_matcher.py
@@ -5,8 +5,9 @@ from .exceptions import LanguageNotSupported
 from .translating import translate
 
 
-class SimpleWord2VecMatcher:
-    def __init__(self, debug=True):
+class Word2VecMatcher:
+    """Abstract base class for word2vec matchers."""
+    def __init__(self, debug=False):
         self.models = {}
         self.chunker = SimpleChunker()
         self.debug = debug
@@ -20,14 +21,6 @@ class SimpleWord2VecMatcher:
         self.add_language_model(language, model)
         if self.debug:
             print("Loaded model for {0} from {1}.".format(language, path))
-
-    def match_keyword_sentence(self, model, keyword, sentence: List) -> float:
-        if not keyword or not sentence:
-            return 0.0
-        result = model.wv.n_similarity([keyword], sentence)
-        if self.debug:
-            print("Matching {0} against {1} => {2}".format(keyword, sentence, result))
-        return result
 
     def match_keyword_text(self, model, keyword: str, sentences: List[List[str]], text: str) -> float:
         if keyword in text:
@@ -58,3 +51,36 @@ class SimpleWord2VecMatcher:
         else:
             return 0.0
 
+
+class SentenceVecMatcher(Word2VecMatcher):
+    """Word2vec matcher based on whole sentences."""
+    def match_keyword_sentence(self, model, keyword, sentence: List) -> float:
+        if not keyword or not sentence:
+            return 0.0
+        result = model.wv.n_similarity([keyword], sentence)
+        if self.debug:
+            print("Matching {0} against {1} => {2}".format(keyword, sentence, result))
+        return result
+
+
+class NgramVecMatcher(Word2VecMatcher):
+    """Word2vec matcher based on n-grams."""
+    def __init__(self, n, debug=False):
+        super(NgramVecMatcher, self).__init__(debug=debug)
+        self.n = n
+
+    def match_ngram(self, model, keyword, ngram):
+        return model.wv.n_similarity([keyword], ngram)
+
+    def match_keyword_sentence(self, model, keyword, sentence: List) -> float:
+        from nltk import ngrams
+        if not keyword or not sentence:
+            return 0.0
+        ngram_matches = [self.match_ngram(model, keyword, ngram) for ngram in ngrams(sentence, self.n)]
+        if ngram_matches:
+            result = max(ngram_matches)
+        else:
+            result = 0.0
+        if self.debug:
+            print("Matching {0} against {1} => {2}".format(keyword, sentence, result))
+        return result
