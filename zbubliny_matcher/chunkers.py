41e81784453d6bc51418134acff85620e8680df4
oliver.velich <bielovico@gmail.com>
2017-10-14 05:36:12 +0200

    Moses Chuncker Added with requirements.

--- zbubliny_matcher/chunkers.py
+++ zbubliny_matcher/chunkers.py
@@ -1,6 +1,24 @@
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize.moses import MosesTokenizer
+import iso639
+
 class SimpleChunker:
-    def chunk_words(self, sentence):
+    def chunk_words(self, sentence, language=''):
         return [word.lower() for word in sentence.strip().split()]
 
-    def chunk_sentences(self, text):
+    def chunk_sentences(self, text, language=''):
         return [ t.strip() for t in text.strip().split(".") if t.strip() ]
+
+class MosesChunker():
+    def chunk_sentences(self, text, language='en'):
+        lang = iso639.to_name(language).lower()
+        try:
+            sentences = sent_tokenize(text, lang)
+        except LookupError:
+            sentences = sent_tokenize(text)
+        return sentences
+
+    def chunk_words(self, sentence, language='en'):
+        tokenizer = MosesTokenizer(lang=language)
+        return tokenizer.tokenize(sentence)
+
